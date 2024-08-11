07936bc1026da72b1d88bb82b6d6edcf0d112031
Nemesla <nemesla@yandex.ru>
2024-08-11 23:02:44 +0300

    Add page 404

--- handlers.py
+++ handlers.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from flask.views import MethodView
 
+# Page Index class
 class Index(MethodView):
     @staticmethod
     def get():
         return render_template("index.html")
+    
+# Page error 404 class
+class PageNotFound():
+    @staticmethod
+    def page_not_found(e):
+        return render_template('page404.html'), 404
--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from handlers import Index
+from handlers import Index, PageNotFound
+
 
 class Routes():
     @staticmethod
     def set(app):
         app.add_url_rule("/", view_func=Index.as_view('/'))
+        app.register_error_handler(404, PageNotFound.page_not_found)
         return app
